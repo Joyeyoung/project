01ffa88affc4eb21158734eff053c4f956677476
Joyeyoung <107229223+Joyeyoung@users.noreply.github.com>
2025-06-27 15:54:11 +0900

    Add files via upload

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = require('node-fetch');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -10,8 +10,9 @@ export default async function handler(req, res) {
     return;
   }
 
-  const { query } = req;
-  const params = new URLSearchParams(query).toString();
+  // 쿼리스트링 파싱
+  const urlObj = new URL(req.url, `http://${req.headers.host}`);
+  const params = urlObj.searchParams.toString();
   const url = `https://apis.data.go.kr/1421000/mssBizService_v2/getMssBizList?${params}`;
 
   try {
@@ -21,4 +22,4 @@ export default async function handler(req, res) {
   } catch (e) {
     res.status(500).json({ error: 'API fetch failed', detail: e.message });
   }
-} 
+}; 
